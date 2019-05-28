32edadefb915306275474257aa430f5bac4856a3
Daniel Valcarce <davamix@gmail.com>
2019-05-28 14:54:30 +0200

    Implemented CarSpawner and WeaponSpawner.

---
+++ GTA5RP/RPClient/CarSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+// https://docs.fivem.net/scripting-manual/introduction/creating-your-first-script-csharp/
+
+namespace RPClient
+{
+    class CarSpawner : BaseScript
+    {
+        public CarSpawner()
+        {
+            API.RegisterCommand("car", new Action<int, List<object>, string>(async (source, args, raw) =>
+            {
+                // account for the argument not being passed
+                var model = "adder";
+                if (args.Count > 0)
+                {
+                    model = args[0].ToString();
+                }
+
+                // check if the model actually exists
+                // assumes the directive `using static CitizenFX.Core.Native.API;`
+                var hash = (uint)API.GetHashKey(model);
+                if (!API.IsModelInCdimage(hash) || !API.IsModelAVehicle(hash))
+                {
+                    TriggerEvent("chat:addMessage", new
+                    {
+                        color = new[] { 255, 0, 0 },
+                        args = new[] { "[CarSpawner]", $"It might have been a good thing that you tried to spawn a {model}. Who even wants their spawning to actually ^*succeed?" }
+                    });
+                    return;
+                }
+
+                // create the vehicle
+                var vehicle = await World.CreateVehicle(model, Game.PlayerPed.Position, Game.PlayerPed.Heading);
+
+                // set the player ped into the vehicle and driver seat
+                Game.PlayerPed.SetIntoVehicle(vehicle, VehicleSeat.Driver);
+
+                // tell the player
+                TriggerEvent("chat:addMessage", new
+                {
+                    color = new[] { 255, 0, 0 },
+                    args = new[] { "[CarSpawner]", $"Woohoo! Enjoy your new ^*{model}!" }
+                });
+            }), false);
+        }
+    }
+}
---
+++ GTA5RP/RPClient/WeaponSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+// https://www.youtube.com/watch?v=drHqAE4sITg
+
+namespace RPClient
+{
+    public class WeaponSpawner : BaseScript
+    {
+        public WeaponSpawner()
+        {
+            // "giveweapon" -> command name
+            // src -> source player ID
+            // args -> command arguments
+            // raw -> raw command
+            API.RegisterCommand("giveweapon", new Action<int, List<object>, string>((src, args, raw) =>
+            {
+                var argList = args.Select(o => o.ToString()).ToList();
+                if (argList.Any() && Enum.TryParse(argList[0], true, out WeaponHash weapon))
+                {
+                    Game.PlayerPed.Weapons.Give(weapon, 999, true, true);
+                }
+            }), false);
+        }
+
+        public static void SendChatMessage(string title, string message, int r, int g, int b)
+        {
+            var msg = new Dictionary<string, object>
+            {
+                ["color"] = new[] { r, g, b },
+                ["args"] = new[] { title, message }
+            };
+
+            TriggerEvent("chat:addMessage", msg);
+        }
+    }
+}
